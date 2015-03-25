1366614b8bbe8d7a139075cc53350a2afe2984ea
Filipe <snkchrist@gmail.com>
2015-03-24 21:43:57 -0300

    Added balanco model

--- Fundamentus/src/com/snk/fundamentus/XlsReader.java
+++ Fundamentus/src/com/snk/fundamentus/XlsReader.java
@@ -3,6 +3,9 @@ package com.snk.fundamentus;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -13,7 +16,7 @@ import com.snk.fundamentus.models.BalancoPatrimonial;
 
 public class XlsReader {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         FileInputStream file = new FileInputStream(new File("H:\\balanco.xlsx"));
 
         XSSFWorkbook workbook = new XSSFWorkbook(file);
@@ -23,29 +26,184 @@ public class XlsReader {
         // Iterate through each rows one by one
         Iterator<Row> rowIterator = sheet.iterator();
 
-        BalancoPatrimonial balanco = new BalancoPatrimonial();
+        int maximumNumberOfCells = 32;
 
-        while (rowIterator.hasNext()) {
-            Row row = rowIterator.next();
+        for (int i = 1; i <= maximumNumberOfCells; i++) {
+            BalancoPatrimonial balanco = new BalancoPatrimonial();
 
-            if (row.getRowNum() < 1)
-                continue;
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                // row.getPhysicalNumberOfCells();
+                if (row.getRowNum() < 1)
+                    continue;
 
-            Cell cell = row.getCell(0);
+                Cell cell = row.getCell(0);
 
-            if (null != cell) {
+                if (null != cell) {
+                    String stringValueFromCell = cell.getStringCellValue();
+                    double numericValueFromCell = 0.0;
+                    try {
+                        String stringFromCell = row.getCell(i).getStringCellValue().replace(".", "");
+                        double parseDouble = Double.parseDouble(stringFromCell);
+                        numericValueFromCell = parseDouble;
+                    }
+                    catch (Exception e) {
+                        System.out.println("An error occured");
+                    }
 
-                String stringCellValue = cell.getStringCellValue();
+                    if (stringValueFromCell.equals("Ativo Total"))
+                        balanco.setAtivoTotal(numericValueFromCell);
 
-                if (stringCellValue.equals("Ativo Total")) {
-                    balanco.setAtivoCirculante(row.getCell(1).getNumericCellValue() * 1000);
+                    if (stringValueFromCell.equals("Adiantamento para Futuro Aumento Capital"))
+                        balanco.setAdiantamentoParaFuturoAumentoCapital(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Ajustes Acumulados de Conversão"))
+                        balanco.setAjustesAcumuladosDeConversao(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Ajustes de Avaliação Patrimonial"))
+                        balanco.setAjustesDeAvaliacaoPatrimonial(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Aplicações Financeiras"))
+                        balanco.setAplicacoesFinanceiras(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Aplicações Financeiras Avaliadas a Valor Justo"))
+                        balanco.setAplicacoesFinanceirasAvaliadasAValorJusto(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Aplicações Financeiras Avaliadas ao Custo Amortizado"))
+                        balanco.setAplicacoesFinanceirasAvaliadasAoCustoAmortizado(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Ativo Circulante"))
+                        balanco.setAtivoCirculante(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Ativo Realizável a Longo Prazo"))
+                        balanco.setAtivoRealizavelALongoPrazo(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Ativos Biológicos"))
+                        balanco.setAtivosBiologicos(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Caixa e Equivalentes de Caixa"))
+                        balanco.setCaixaEEquivalentesDeCaixa(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Capital Social Realizado"))
+                        balanco.setCapitalSocialRealizado(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Contas a Receber"))
+                        balanco.setContasAReceber(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Créditos com Partes Relacionadas"))
+                        balanco.setCreditosComPartesRelacionadas(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Despesas Antecipadas"))
+                        balanco.setDespesasAntecipadas(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Diferido"))
+                        balanco.setDiferido(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Dividendos e JCP a Pagar"))
+                        balanco.setDividendosEJCPAPagar(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Empréstimos e Financiamentos"))
+                        balanco.setEmprestimosEFinanciamentos(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Estoques"))
+                        balanco.setEstoques(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Fornecedores"))
+                        balanco.setFornecedores(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Imobilizado"))
+                        balanco.setImobilizado(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Intangível"))
+                        balanco.setIntangivel(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Investimentos"))
+                        balanco.setInvestimentos(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Lucros e Receitas a Apropriar"))
+                        balanco.setLucrosEReceitasAApropriar(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Lucros/Prejuízos Acumulados"))
+                        balanco.setLucros_PrejuizosAcumulados(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Obrigações Fiscais"))
+                        balanco.setObrigacoesFiscais(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Obrigações Sociais e Trabalhistas"))
+                        balanco.setObrigacoesSociaisETrabalhistas(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Outros"))
+                        balanco.setOutros(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Outros Ativos Circulantes"))
+                        balanco.setOutrosAtivosCirculantes(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Outros Ativos Não Circulantes"))
+                        balanco.setOutrosAtivosNaoCirculantes(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Outros Resultados Abrangentes"))
+                        balanco.setOutrosResultadosAbrangentes(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Participação dos Acionistas Não Controladores"))
+                        balanco.setParticipacaoDosAcionistasNaoControladores(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Passivo Circulante"))
+                        balanco.setPassivoCirculante(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Passivo Não Circulante"))
+                        balanco.setPassivoNaoCirculante(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Passivo Total"))
+                        balanco.setPassivoTotal(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Passivos com Partes Relacionadas"))
+                        balanco.setPassivosComPartesRelacionadas(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Passivos sobre Ativos Não-Correntes a Venda e Descontinuados"))
+                        balanco.setPassivosSobreAtivosNaoCorrentesAVendaEDescontinuados(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Patrimônio Líquido"))
+                        balanco.setPatrimônioLiquido(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Provisões"))
+                        balanco.setProvisoes(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Reservas de Capital"))
+                        balanco.setReservasDeCapital(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Reservas de Lucros"))
+                        balanco.setReservasDeLucros(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Reservas de Reavaliação"))
+                        balanco.setReservasDeReavaliacao(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Tributos a Recuperar"))
+                        balanco.setTributosARecuperar(numericValueFromCell);
+
+                    if (stringValueFromCell.equals("Tributos Diferidos"))
+                        balanco.setTributosDiferidos(numericValueFromCell);
+
                 }
+                else {
+                    // int physicalNumberOfCells =
+                    // row.getPhysicalNumberOfCells();
+                    String stringCellValue = row.getCell(i).getStringCellValue();
+
+                    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+                    Date parse = format.parse(stringCellValue);
+
+                    balanco.setDataDoBalanco(parse);
+
+                }
+
+                // case Cell.CELL_TYPE_STRING:
+                // case Cell.CELL_TYPE_NUMERIC:
+
             }
 
-            // case Cell.CELL_TYPE_STRING:
-            // case Cell.CELL_TYPE_NUMERIC:
-
+            System.out.println("Done with " + balanco.getDataDoBalanco());
         }
+
         file.close();
     }
 }
--- Fundamentus/src/com/snk/fundamentus/models/BalancoPatrimonial.java
+++ Fundamentus/src/com/snk/fundamentus/models/BalancoPatrimonial.java
@@ -1,7 +1,46 @@
 package com.snk.fundamentus.models;
 
+import java.util.Date;
+
+
 public class BalancoPatrimonial {
 
+    public double adiantamentoParaFuturoAumentoCapital;
+    public double ajustesAcumuladosDeConversao;
+    public double ajustesDeAvaliacaoPatrimonial;
+    public double aplicacoesFinanceiras;
+    public double aplicacoesFinanceirasAvaliadasAValorJusto;
+    public double aplicacoesFinanceirasAvaliadasAoCustoAmortizado;
+    public double ativosBiologicos;
+    public double caixaEEquivalentesDeCaixa;
+    public double capitalSocialRealizado;
+    public double contasAReceber;
+    public double creditosComPartesRelacionadas;
+    public double despesasAntecipadas;
+    public double diferido;
+    public double dividendosEJCPAPagar;
+    public double emprestimosEFinanciamentos;
+    public double estoques;
+    public double fornecedores;
+    public double imobilizado;
+    public double intangivel;
+    public double investimentos;
+    public double lucrosEReceitasAApropriar;
+    public double lucros_PrejuizosAcumulados;
+    public double obrigacoesFiscais;
+    public double obrigacoesSociaisETrabalhistas;
+    public double outros;
+    public double outrosResultadosAbrangentes;
+    public double participacaoDosAcionistasNaoControladores;
+    public double passivosComPartesRelacionadas;
+    public double passivosSobreAtivosNaoCorrentesAVendaEDescontinuados;
+    public double patrimônioLiquido;
+    public double provisoes;
+    public double reservasDeCapital;
+    public double reservasDeLucros;
+    public double reservasDeReavaliacao;
+    public double tributosARecuperar;
+    public double tributosDiferidos;
     public double ativoTotal;
     public double ativoCirculante;
     public double outrosAtivosCirculantes;
@@ -10,13 +49,315 @@ public class BalancoPatrimonial {
     public double passivoTotal;
     public double passivoCirculante;
     public double passivoNaoCirculante;
+    public Date dataDoBalanco;
+
+    public double getAdiantamentoParaFuturoAumentoCapital() {
+        return adiantamentoParaFuturoAumentoCapital;
+    }
+
+    public void setAdiantamentoParaFuturoAumentoCapital(
+        double adiantamentoParaFuturoAumentoCapital) {
+        this.adiantamentoParaFuturoAumentoCapital += adiantamentoParaFuturoAumentoCapital;
+    }
+
+    public double getAjustesAcumuladosDeConversao() {
+        return ajustesAcumuladosDeConversao;
+    }
+
+    public void setAjustesAcumuladosDeConversao(double ajustesAcumuladosDeConversao) {
+        this.ajustesAcumuladosDeConversao += ajustesAcumuladosDeConversao;
+    }
+
+    public double getAjustesDeAvaliacaoPatrimonial() {
+        return ajustesDeAvaliacaoPatrimonial;
+    }
+
+    public void setAjustesDeAvaliacaoPatrimonial(double ajustesDeAvaliacaoPatrimonial) {
+        this.ajustesDeAvaliacaoPatrimonial += ajustesDeAvaliacaoPatrimonial;
+    }
+
+    public double getAplicacoesFinanceiras() {
+        return aplicacoesFinanceiras;
+    }
+
+    public void setAplicacoesFinanceiras(double aplicacoesFinanceiras) {
+        this.aplicacoesFinanceiras += aplicacoesFinanceiras;
+    }
+
+    public double getAplicacoesFinanceirasAvaliadasAValorJusto() {
+        return aplicacoesFinanceirasAvaliadasAValorJusto;
+    }
+
+    public void setAplicacoesFinanceirasAvaliadasAValorJusto(
+        double aplicacoesFinanceirasAvaliadasAValorJusto) {
+        this.aplicacoesFinanceirasAvaliadasAValorJusto += aplicacoesFinanceirasAvaliadasAValorJusto;
+    }
+
+    public double getAplicacoesFinanceirasAvaliadasAoCustoAmortizado() {
+        return aplicacoesFinanceirasAvaliadasAoCustoAmortizado;
+    }
+
+    public void setAplicacoesFinanceirasAvaliadasAoCustoAmortizado(
+        double aplicacoesFinanceirasAvaliadasAoCustoAmortizado) {
+        this.aplicacoesFinanceirasAvaliadasAoCustoAmortizado += aplicacoesFinanceirasAvaliadasAoCustoAmortizado;
+    }
+
+    public double getAtivosBiologicos() {
+        return ativosBiologicos;
+    }
+
+    public void setAtivosBiologicos(double ativosBiologicos) {
+        this.ativosBiologicos += ativosBiologicos;
+    }
+
+    public double getCaixaEEquivalentesDeCaixa() {
+        return caixaEEquivalentesDeCaixa;
+    }
+
+    public void setCaixaEEquivalentesDeCaixa(double caixaEEquivalentesDeCaixa) {
+        this.caixaEEquivalentesDeCaixa += caixaEEquivalentesDeCaixa;
+    }
+
+    public double getCapitalSocialRealizado() {
+        return capitalSocialRealizado;
+    }
+
+    public void setCapitalSocialRealizado(double capitalSocialRealizado) {
+        this.capitalSocialRealizado += capitalSocialRealizado;
+    }
+
+    public double getContasAReceber() {
+        return contasAReceber;
+    }
+
+    public void setContasAReceber(double contasAReceber) {
+        this.contasAReceber += contasAReceber;
+    }
+
+    public double getCreditosComPartesRelacionadas() {
+        return creditosComPartesRelacionadas;
+    }
+
+    public void setCreditosComPartesRelacionadas(double creditosComPartesRelacionadas) {
+        this.creditosComPartesRelacionadas += creditosComPartesRelacionadas;
+    }
+
+    public double getDespesasAntecipadas() {
+        return despesasAntecipadas;
+    }
+
+    public void setDespesasAntecipadas(double despesasAntecipadas) {
+        this.despesasAntecipadas += despesasAntecipadas;
+    }
+
+    public double getDiferido() {
+        return diferido;
+    }
+
+    public void setDiferido(double diferido) {
+        this.diferido += diferido;
+    }
+
+    public double getDividendosEJCPAPagar() {
+        return dividendosEJCPAPagar;
+    }
+
+    public void setDividendosEJCPAPagar(double dividendosEJCPAPagar) {
+        this.dividendosEJCPAPagar += dividendosEJCPAPagar;
+    }
+
+    public double getEmprestimosEFinanciamentos() {
+        return emprestimosEFinanciamentos;
+    }
+
+    public void setEmprestimosEFinanciamentos(double emprestimosEFinanciamentos) {
+        this.emprestimosEFinanciamentos += emprestimosEFinanciamentos;
+    }
+
+    public double getEstoques() {
+        return estoques;
+    }
+
+    public void setEstoques(double estoques) {
+        this.estoques += estoques;
+    }
+
+    public double getFornecedores() {
+        return fornecedores;
+    }
+
+    public void setFornecedores(double fornecedores) {
+        this.fornecedores += fornecedores;
+    }
+
+    public double getImobilizado() {
+        return imobilizado;
+    }
+
+    public void setImobilizado(double imobilizado) {
+        this.imobilizado += imobilizado;
+    }
+
+    public double getIntangivel() {
+        return intangivel;
+    }
+
+    public void setIntangivel(double intangivel) {
+        this.intangivel += intangivel;
+    }
+
+    public double getInvestimentos() {
+        return investimentos;
+    }
+
+    public void setInvestimentos(double investimentos) {
+        this.investimentos += investimentos;
+    }
+
+    public double getLucrosEReceitasAApropriar() {
+        return lucrosEReceitasAApropriar;
+    }
+
+    public void setLucrosEReceitasAApropriar(double lucrosEReceitasAApropriar) {
+        this.lucrosEReceitasAApropriar += lucrosEReceitasAApropriar;
+    }
+
+    public double getLucros_PrejuizosAcumulados() {
+        return lucros_PrejuizosAcumulados;
+    }
+
+    public void setLucros_PrejuizosAcumulados(double lucros_PrejuizosAcumulados) {
+        this.lucros_PrejuizosAcumulados += lucros_PrejuizosAcumulados;
+    }
+
+    public double getObrigacoesFiscais() {
+        return obrigacoesFiscais;
+    }
+
+    public void setObrigacoesFiscais(double obrigacoesFiscais) {
+        this.obrigacoesFiscais += obrigacoesFiscais;
+    }
+
+    public double getObrigacoesSociaisETrabalhistas() {
+        return obrigacoesSociaisETrabalhistas;
+    }
+
+    public void setObrigacoesSociaisETrabalhistas(double obrigacoesSociaisETrabalhistas) {
+        this.obrigacoesSociaisETrabalhistas += obrigacoesSociaisETrabalhistas;
+    }
+
+    public double getOutros() {
+        return outros;
+    }
+
+    public void setOutros(double outros) {
+        this.outros += outros;
+    }
+
+    public double getOutrosResultadosAbrangentes() {
+        return outrosResultadosAbrangentes;
+    }
+
+    public void setOutrosResultadosAbrangentes(double outrosResultadosAbrangentes) {
+        this.outrosResultadosAbrangentes += outrosResultadosAbrangentes;
+    }
+
+    public double getParticipacaoDosAcionistasNaoControladores() {
+        return participacaoDosAcionistasNaoControladores;
+    }
+
+    public void setParticipacaoDosAcionistasNaoControladores(
+        double participacaoDosAcionistasNaoControladores) {
+        this.participacaoDosAcionistasNaoControladores += participacaoDosAcionistasNaoControladores;
+    }
+
+    public double getPassivosComPartesRelacionadas() {
+        return passivosComPartesRelacionadas;
+    }
+
+    public void setPassivosComPartesRelacionadas(double passivosComPartesRelacionadas) {
+        this.passivosComPartesRelacionadas += passivosComPartesRelacionadas;
+    }
+
+    public double getPassivosSobreAtivosNaoCorrentesAVendaEDescontinuados() {
+        return passivosSobreAtivosNaoCorrentesAVendaEDescontinuados;
+    }
+
+    public void setPassivosSobreAtivosNaoCorrentesAVendaEDescontinuados(
+        double passivosSobreAtivosNaoCorrentesAVendaEDescontinuados) {
+        this.passivosSobreAtivosNaoCorrentesAVendaEDescontinuados += passivosSobreAtivosNaoCorrentesAVendaEDescontinuados;
+    }
+
+    public double getPatrimônioLiquido() {
+        return patrimônioLiquido;
+    }
+
+    public void setPatrimônioLiquido(double patrimônioLiquido) {
+        this.patrimônioLiquido += patrimônioLiquido;
+    }
+
+    public double getProvisoes() {
+        return provisoes;
+    }
+
+    public void setProvisoes(double provisoes) {
+        this.provisoes += provisoes;
+    }
+
+    public double getReservasDeCapital() {
+        return reservasDeCapital;
+    }
+
+    public void setReservasDeCapital(double reservasDeCapital) {
+        this.reservasDeCapital += reservasDeCapital;
+    }
+
+    public double getReservasDeLucros() {
+        return reservasDeLucros;
+    }
+
+    public void setReservasDeLucros(double reservasDeLucros) {
+        this.reservasDeLucros += reservasDeLucros;
+    }
+
+    public double getReservasDeReavaliacao() {
+        return reservasDeReavaliacao;
+    }
+
+    public void setReservasDeReavaliacao(double reservasDeReavaliacao) {
+        this.reservasDeReavaliacao += reservasDeReavaliacao;
+    }
+
+    public double getTributosARecuperar() {
+        return tributosARecuperar;
+    }
+
+    public void setTributosARecuperar(double tributosARecuperar) {
+        this.tributosARecuperar += tributosARecuperar;
+    }
+
+    public double getTributosDiferidos() {
+        return tributosDiferidos;
+    }
+
+    public void setTributosDiferidos(double tributosDiferidos) {
+        this.tributosDiferidos += tributosDiferidos;
+    }
+
+    public Date getDataDoBalanco() {
+        return dataDoBalanco;
+    }
+
+    public void setDataDoBalanco(Date dataDoBalanco) {
+        this.dataDoBalanco = dataDoBalanco;
+    }
 
     public double getAtivoTotal() {
         return ativoTotal;
     }
 
     public void setAtivoTotal(double ativoTotal) {
-        this.ativoTotal = ativoTotal;
+        this.ativoTotal += ativoTotal;
     }
 
     public double getAtivoCirculante() {
@@ -24,7 +365,7 @@ public class BalancoPatrimonial {
     }
 
     public void setAtivoCirculante(double ativoCirculante) {
-        this.ativoCirculante = ativoCirculante;
+        this.ativoCirculante += ativoCirculante;
     }
 
     public double getOutrosAtivosCirculantes() {
@@ -32,7 +373,7 @@ public class BalancoPatrimonial {
     }
 
     public void setOutrosAtivosCirculantes(double outrosAtivosCirculantes) {
-        this.outrosAtivosCirculantes = outrosAtivosCirculantes;
+        this.outrosAtivosCirculantes += outrosAtivosCirculantes;
     }
 
     public double getAtivoRealizavelALongoPrazo() {
@@ -40,7 +381,7 @@ public class BalancoPatrimonial {
     }
 
     public void setAtivoRealizavelALongoPrazo(double ativoRealizavelALongoPrazo) {
-        this.ativoRealizavelALongoPrazo = ativoRealizavelALongoPrazo;
+        this.ativoRealizavelALongoPrazo += ativoRealizavelALongoPrazo;
     }
 
     public double getOutrosAtivosNaoCirculantes() {
@@ -48,7 +389,7 @@ public class BalancoPatrimonial {
     }
 
     public void setOutrosAtivosNaoCirculantes(double outrosAtivosNaoCirculantes) {
-        this.outrosAtivosNaoCirculantes = outrosAtivosNaoCirculantes;
+        this.outrosAtivosNaoCirculantes += outrosAtivosNaoCirculantes;
     }
 
     public double getPassivoTotal() {
@@ -56,7 +397,7 @@ public class BalancoPatrimonial {
     }
 
     public void setPassivoTotal(double passivoTotal) {
-        this.passivoTotal = passivoTotal;
+        this.passivoTotal += passivoTotal;
     }
 
     public double getPassivoCirculante() {
@@ -64,7 +405,7 @@ public class BalancoPatrimonial {
     }
 
     public void setPassivoCirculante(double passivoCirculante) {
-        this.passivoCirculante = passivoCirculante;
+        this.passivoCirculante += passivoCirculante;
     }
 
     public double getPassivoNaoCirculante() {
@@ -72,7 +413,7 @@ public class BalancoPatrimonial {
     }
 
     public void setPassivoNaoCirculante(double passivoNaoCirculante) {
-        this.passivoNaoCirculante = passivoNaoCirculante;
+        this.passivoNaoCirculante += passivoNaoCirculante;
     }
 
 }
