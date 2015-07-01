5984fb657b65f3f37217551d6b94f3743cc58079
Filipe Ribeiro <filipe.ftec@gmail.com>
2015-07-01 18:13:03 -0300

    adding cash flow constrains

--- Fundamentus/src/com/snk/fundamentus/report/CashFlow.java
+++ Fundamentus/src/com/snk/fundamentus/report/CashFlow.java
@@ -43,14 +43,68 @@ public class CashFlow {
         return fluxo;
     }
 
-    public static void main(final String[] args) {
-        //percentual
-        double taxaDesconto = 15;
-        double taxaPerpetuidade = 6;
+    public double getTaxaDesconto(
+            final double beta,
+            final double custoDividaEmpresa,
+            final double percentualRecursosProprios,
+            final double percentualRecursosTerceiros) {
 
+        /**
+         * TODAS AS TAXAS SÃO PARA OS ÚLTIMOS 12 MESES
+         */
+        double selic = 13.75;
+        double poupanca = 7.39;
+        double cdi = 11.58;
+        double titulos = 12.01;
+        double ibovespa = 1.27;
+        double inflacao = 8.47;
+        double media = (selic + poupanca + cdi + titulos + ibovespa) / 5;
+
+        ///retornoEsperadoSobreIndiceMercado
+        double rm = media + inflacao;
+
+        //retorno Ativo Livre de Risco (usada a TJLP dos últimos 12 meses)
+        double rf = 5.45;
+
+        //custo de capital próprio = KE
+        double custoCapitalProprio = rf + beta * (rm - rf);
+
+        double a1 = percentualRecursosProprios * custoCapitalProprio;
+        double a2 = percentualRecursosTerceiros * custoDividaEmpresa;
+
+        double taxaDesconto = (a1 + a2);
+        System.out.println("A taxa de desconto é de = " + taxaDesconto);
+
+        return taxaDesconto;
+
+    }
+
+    public static void main(final String[] args) {
         CashFlow flow = new CashFlow();
-        double computed = flow.computeDiscountedCashFlow(3013804000D, taxaDesconto, taxaPerpetuidade);
+
+        //RiscoNegocio
+        //crescimento esperado sobre o mercado 1 vez e 0.64 sobre o setor
+        //exemplo o setor vai crescer 9% neste ano e a empresa espera um crescimento de 14.80
+        // 14.80 - 9 = 5,8
+        // 5,8 / 9 = 0,64
+        // 1 + 0,64 = 1.64
+        double beta = 1.64;
+        //Taxa de juros sobre o capital de terceiros
+        double custoDividaEmpresa = 6.2;
+
+        //QUANTO DE RECURSOS PROPRIOS SERÁ INVESTIDO NA EMPRESA E QUANDO DE RECURSOS DE TERCEIROS (valor percentual)
+        double recursosProprios = 0.7828;
+        double recursosTerceiros = 0.2171;
+
+        double taxaDesconto = flow.getTaxaDesconto(beta, custoDividaEmpresa, recursosProprios, recursosTerceiros);
+
+        //crecimento lucro liquido media de 5 anos
+        double taxaPerpetuidade = 11.60;
+        double fluxoDeCaixa2015 = 3013804000D;
         int numeroAcoesEmitidas = 1614350000;
+
+        double computed = flow.computeDiscountedCashFlow(fluxoDeCaixa2015, taxaDesconto, taxaPerpetuidade);
+
         double valorJustoAcao = computed / numeroAcoesEmitidas;
 
         System.out.println("Valor da empresa [" + flow.convert(computed) + "]");
